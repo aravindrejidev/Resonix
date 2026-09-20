ec407e5dd879c21fb54077033650a2627472b8a3
Aravind Reji <aravindreji.dev@gmail.com>
2026-09-20 08:47:57 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.ksp)
 }
 
 android {
@@ -85,4 +86,8 @@ dependencies {
     implementation(libs.androidx.material.icons.extended)
 
     implementation(libs.oboe)
+
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.ktx)
+    ksp(libs.androidx.room.compiler)
 }
